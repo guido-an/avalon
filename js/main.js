703f49dbfa0369401ce6413d805b964ce7583841
Guido <carucciguido@gmail.com>
2020-07-13 09:34:30 +0200

    nuova pagina terra mia

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
 
-$(document).ready(function(){
-
+$(document).ready(function () {
   // From mobile show menu when click on burger icon
-  $("#mobile-menu").click( function() {
-    $("#menu").toggle('');
+  $('#mobile-menu').click(function () {
+    $('#menu').toggle('')
   })
 
-  $("#menu").click(function(){
-	$("#nav").toggle();
-});
+  $('#menu').click(function () {
+    $('#nav').toggle()
+  })
 
-
-  AOS.init(); // initialize animations effect https://michalsnik.github.io/aos/ 
-
-
+  AOS.init() // initialize animations effect https://michalsnik.github.io/aos/
 }) // end of ready function
