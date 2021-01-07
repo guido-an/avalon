b0fa0192c821d57d607c6db6d0456699e05acf38
Guido Carucci <carucciguido@gmail.com>
2021-01-07 12:00:55 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,8 +12,3 @@ $(document).ready(function () {
   AOS.init() // initialize animations effect https://michalsnik.github.io/aos/
 }) // end of ready function
 
-var alerted = localStorage.getItem('alerted') || ''
-if (alerted != 'yes') {
-  alert('Dal 19 Ottobre al 5 Novembre saremo chiusi per ferie. ')
-  localStorage.setItem('alerted', 'yes')
-}
